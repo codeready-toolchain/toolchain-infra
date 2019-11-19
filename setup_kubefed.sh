0449980c9872d9baeb8a92fee51261338d1e68e8
Dipak Pawar <dipakpawar231@gmail.com>
2019-11-19 09:06:29 +0530

    update toolchain-common to master

--- setup_kubefed.sh
+++ setup_kubefed.sh
@@ -19,8 +19,8 @@ function setup_kubefed() {
   sed -i -e "s/name: admin/name: member-admin/g; s/user: admin/user: member-admin/g" $PWD/member-config
 
   export KUBECONFIG=$PWD/host-config:$PWD/member-config
-  curl -sSL https://raw.githubusercontent.com/dipak-pawar/toolchain-common/c81d071953d42558c2945e53b221678a2a5b3047/scripts/add-cluster.sh | bash -s -- -t member -mn ${MEMBER_OPERATOR_NS} -hn ${HOST_OPERATOR_NS} -kc ${KUBECONFIG}
-  curl -sSL https://raw.githubusercontent.com/dipak-pawar/toolchain-common/c81d071953d42558c2945e53b221678a2a5b3047/scripts/add-cluster.sh | bash -s -- -t host -mn ${MEMBER_OPERATOR_NS} -hn ${HOST_OPERATOR_NS} -kc ${KUBECONFIG}
+  curl -sSL https://raw.githubusercontent.com/codeready-toolchain/toolchain-common/master/scripts/add-cluster.sh | bash -s -- -t member -mn ${MEMBER_OPERATOR_NS} -hn ${HOST_OPERATOR_NS} -kc ${KUBECONFIG}
+  curl -sSL https://raw.githubusercontent.com/codeready-toolchain/toolchain-common/master/scripts/add-cluster.sh | bash -s -- -t host -mn ${MEMBER_OPERATOR_NS} -hn ${HOST_OPERATOR_NS} -kc ${KUBECONFIG}
 }
 
 function wait_for_resources_to_exists() {
