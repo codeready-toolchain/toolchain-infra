096e43a8ff0270ccd6464abaa015475eb10b94e9
Alexey Kazakov <alkazako@redhat.com>
2019-12-06 07:46:11 -0800

    Add Nurali

--- create_users.sh
+++ create_users.sh
@@ -7,7 +7,7 @@ function create_users() {
     [mjobanek-crtadmin]=52791559
     [xcoulon-crtadmin]=52753083
     [sbryzak-crtadmin]=53039915
-    #[nvirani-crtadmin]=
+    [nvirani-crtadmin]=52791165
     #[tkurian-crtadmin]=
   )
   USERS=""
