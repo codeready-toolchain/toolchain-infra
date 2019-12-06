0dfac669e9a4c8d54d7d558a87d23472178466f4
Alexey Kazakov <alkazako@redhat.com>
2019-12-05 16:07:29 -0800

    Add Xavier and Matous

--- create_users.sh
+++ create_users.sh
@@ -2,11 +2,10 @@ function create_users() {
   declare -A users
   users=(
     [alkazako-crtadmin]=52787588
-    #[dpawar-crtadmin]=18c2b531-50a0-4ce9-95bb-2090db5feca1
-    #[mjobanek-crtadmin]=18858c97-3bd4-43fe-9aa8-14ce22265119
-    #[nvirani-crtadmin]=2be89467-f2f1-4e14-b1ae-3bf84c8cef52
-    #[tkurian-crtadmin]=88b195e7-b064-41b7-8fb5-67d640c3703c
-    #[xcoulon-crtadmin]=b82cedcb-1600-4d0a-a49c-0e149e626733
+    [mjobanek-crtadmin]=52791559
+    [xcoulon-crtadmin]=52753083
+    #[nvirani-crtadmin]=
+    #[tkurian-crtadmin]=
   )
   USERS=""
   for i in "${!users[@]}"; do
