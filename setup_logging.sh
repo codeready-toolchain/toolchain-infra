41236860eb22995514c27b783ce5bd8b3fd82707
Xavier Coulon <xcoulon@redhat.com>
2020-01-13 11:22:45 +0100

    apply review comments
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- setup_logging.sh
+++ setup_logging.sh
@@ -4,33 +4,26 @@ function setup_logging() {
     echo "installing Cluster Logging and EFK operators..."
     oc create -f cluster-logging/eo-namespace.yaml
     oc create -f cluster-logging/clo-namespace.yaml
-    oc project openshift-operators-redhat
     oc create -f cluster-logging/eo-og.yaml
     oc create -f cluster-logging/eo-sub.yaml
-    oc create -f cluster-logging/eo-rbac.yaml
-    oc project openshift-logging
     oc create -f cluster-logging/clo-og.yaml
     oc create -f cluster-logging/clo-sub.yaml
     # wait until the CRD is available before creating the CR
     NEXT_WAIT_TIME=0
-    while [[ -z `oc get crd clusterloggings.logging.openshift.io 2>/dev/null` ]]; do \
+    while [[ -z `oc get crd clusterloggings.logging.openshift.io 2>/dev/null` ]]; do
 		if [[ ${NEXT_WAIT_TIME} -eq 300 ]]; then \
-		   OPERATOR_GROUP_NAME=`oc get operatorgroups --output=name -n openshift-logging | grep "openshift-logging-"`; \
-		   SUBSCRIPTION_NAME=`oc get subscription --output=name -n openshift-logging | grep "cluster-logging"`; \
-		   echo "reached timeout of waiting for CRD clusterloggings.logging.openshift.io to be available in the cluster - see following info for debugging:"; \
-		   echo "================================ Operator Group =================================="; \
-		   oc get ${OPERATOR_GROUP_NAME} -n openshift-logging -o yaml; \
-		   echo "================================ Subscription =================================="; \
-		   oc get ${SUBSCRIPTION_NAME} -n openshift-logging -o yaml; \
-		   exit 1; \
-		fi; \
-		echo "$(( NEXT_WAIT_TIME++ )). attempt of waiting for CRD clusterloggings.logging.openshift.io to be available in the cluster..."; \
-		sleep 1; \
+		   OPERATOR_GROUP_NAME=`oc get operatorgroups --output=name -n openshift-logging | grep "openshift-logging-"`
+		   SUBSCRIPTION_NAME=`oc get subscription --output=name -n openshift-logging | grep "cluster-logging"`
+		   echo "reached timeout of waiting for CRD clusterloggings.logging.openshift.io to be available in the cluster - see following info for debugging:"
+		   echo "================================ Subscription =================================="
+		   oc get ${SUBSCRIPTION_NAME} -n openshift-logging -o yaml
+		   exit 1
+		fi
+		echo "$(( NEXT_WAIT_TIME++ )). attempt of waiting for CRD clusterloggings.logging.openshift.io to be available in the cluster..."
+		sleep 1
 	done
-		
-
     oc create -f cluster-logging/instance.yaml
-
+    echo "you can run 'oc get routes --namespace=openshift-logging' to get the URL of the dashboard once the 'kibana' pods are available in this namespace"
 }
 
 setup_logging
