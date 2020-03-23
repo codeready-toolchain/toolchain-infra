56e29676b9cb37c8e7393a1827ad880992d5329a
Alexey Kazakov <alkazako@redhat.com>
2020-03-23 12:10:42 -0700

    Add script for renewing certs (#11)

--- install_certs.sh
+++ install_certs.sh
@@ -1,46 +1,86 @@
 #!/usr/bin/env bash
 
-## Make sure you have requested cert for the clusters domains before running this script:
-## See https://blog.openshift.com/requesting-and-installing-lets-encrypt-certificates-for-openshift-4/ for details
-#
-# cd $HOME
-# git clone https://github.com/neilpang/acme.sh
-# cd acme.sh
-# <Update the file $HOME/acme.sh/dnsapi/dns_aws.sh with your AWS access credentials...>
-# 
-## Then for each cluster:
-# export LE_API=$(oc whoami --show-server | cut -f 2 -d ':' | cut -f 3 -d '/' | sed 's/-api././')
-# export LE_WILDCARD=$(oc get ingresscontroller default -n openshift-ingress-operator -o jsonpath='{.status.domain}')
-# ${HOME}/acme.sh/acme.sh --issue -d ${LE_API} -d *.${LE_WILDCARD} --dns dns_aws
+user_help() {
+  echo "Install or Renew Let's Ecrypt Certificates"
+  echo "Make sure you have acme.sh installed and AWS access credentials are set before before running this script:"
+  echo "(see https://blog.openshift.com/requesting-and-installing-lets-encrypt-certificates-for-openshift-4/ for details)"
+  echo ""
+  echo "  1. Clone the acme.sh GitHub repository:"
+  echo "      cd $HOME"
+  echo "      git clone https://github.com/neilpang/acme.sh"
+  echo "      cd acme.sh"
+  echo "  2. Update the file $HOME/acme.sh/dnsapi/dns_aws.sh with your AWS access credentials:"
+  echo "      #!/usr/bin/env sh"
+  echo "      #AWS_ACCESS_KEY_ID=\"YOUR ACCESS KEY\""
+  echo "      #AWS_SECRET_ACCESS_KEY=\"YOUR SECRET ACCESS KEY\""
+  echo "      #This is the Amazon Route53 api wrapper for acme.sh"
+  echo "      [...]"
+  echo ""
+  echo "options:"
+  echo "-r,  --renew             renew the existing certificates instead of issuing new ones"
+  echo "-h,  --help              print this help message"
+  exit 0
+}
 
-function setup_kubeconfig() {
-  echo "setting up kubeconfig files for both clusters host and member"
-  export KUBECONFIG=$PWD/host-config
-  oc config rename-context admin host-admin
-  sed -i -e "s/name: admin/name: host-admin/g; s/user: admin/user: host-admin/g" $PWD/host-config
-  export KUBECONFIG=$PWD/member-config
-  oc config rename-context admin member-admin
-  sed -i -e "s/name: admin/name: member-admin/g; s/user: admin/user: member-admin/g" $PWD/member-config
+while test $# -gt 0; do
+  case "$1" in
+  -h | --help)
+    user_help
+    ;;
+  -r | --renew)
+    RENEW_CERTS=true
+    shift
+    ;;
+  *)
+    echo "$1 is not a recognized flag!"
+    user_help
+    exit 1
+    ;;
+  esac
+done
 
-  export KUBECONFIG=$PWD/host-config:$PWD/member-config
+function login_to_cluster() {
+  if [[ ${CLUSTER} == "host" ]]; then
+    export KUBECONFIG=$PWD/host-config
+  else
+    export KUBECONFIG=$PWD/member-config
+  fi
 }
 
 function install_cert() {
   export LE_API=$(oc whoami --show-server | cut -f 2 -d ':' | cut -f 3 -d '/' | sed 's/-api././')
   export LE_WILDCARD=$(oc get ingresscontroller default -n openshift-ingress-operator -o jsonpath='{.status.domain}')
 
-  rm -rf tmp
-  mkdir tmp
-  export CERTDIR=$PWD/tmp/certificates
+  if [[ ${RENEW_CERTS} == "true" ]]; then
+    FLAG="--renew"
+  else
+    FLAG="--issue"
+  fi
+  ${HOME}/acme.sh/acme.sh ${FLAG} -d ${LE_API} -d *.${LE_WILDCARD} --dns dns_aws
+
+  export CERTDIR=$PWD/tmp/certificates/${CLUSTER}
+  rm -rf ${CERTDIR} 2> /dev/null
   mkdir -p ${CERTDIR}
   ${HOME}/acme.sh/acme.sh --install-cert -d ${LE_API} -d *.${LE_WILDCARD} --cert-file ${CERTDIR}/cert.pem --key-file ${CERTDIR}/key.pem --fullchain-file ${CERTDIR}/fullchain.pem --ca-file ${CERTDIR}/ca.cer
 
+  if [[ ${RENEW_CERTS} == "true" ]]; then
+    oc delete secret tls router-certs -n openshift-ingress
+  fi
   oc create secret tls router-certs --cert=${CERTDIR}/fullchain.pem --key=${CERTDIR}/key.pem -n openshift-ingress
   oc patch ingresscontroller default -n openshift-ingress-operator --type=merge --patch='{"spec": { "defaultCertificate": { "name": "router-certs" }}}'
 }
 
-setup_kubeconfig
-oc config use-context host-admin
-install_cert
-oc config use-context member-admin
-install_cert
+function install_cert_in_host() {
+  CLUSTER="host"
+  login_to_cluster
+  install_cert
+}
+
+function install_cert_in_member() {
+  CLUSTER="member"
+  login_to_cluster
+  install_cert
+}
+
+install_cert_in_host
+install_cert_in_member
