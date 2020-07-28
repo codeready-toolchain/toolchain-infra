3a151c65d8f0c6c605786cd574a7042dd62b6a49
tinakurian <tkurian@redhat.com>
2020-07-28 14:01:47 -0400

    update

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -76,7 +76,19 @@ function deploy_operators() {
   if [[ ${CLUSTER_TYPE} == "host" ]]; then
     oc new-project "$HOST_OPERATOR_NS"
     MAILGUN_DOMAIN=$MAILGUN_DOMAIN MAILGUN_API_KEY=$MAILGUN_API_KEY NAMESPACE=$HOST_OPERATOR_NS envsubst < ./config/host_operator_secret.yaml | oc apply -f -
-    REGISTRATION_SERVICE_URL=$REGISTRATION_SERVICE_URL CONSOLE_NAMESPACE=$CONSOLE_NAMESPACE CHE_NAMESPACE=$CHE_NAMESPACE CHE_NAMESPACED_NAME=$CHE_NAMESPACED_NAME NAMESPACE=$HOST_OPERATOR_NS envsubst < ./config/host_operator_config.yaml | oc apply -f -
+    if [[ -z ${CONSOLE_NAMESPACE} ]]; then
+      export $CONSOLE_NAMESPACE="openshift-console"
+      echo "setting up default CONSOLE_NAMESPACE i.e. $CONSOLE_NAMESPACE "
+    fi
+    if [[ -z ${CHE_NAMESPACE} ]]; then
+      export $CHE_NAMESPACE="toolchain-che"
+      echo "setting up default CHE_NAMESPACE i.e. $CHE_NAMESPACE "
+    fi
+    if [[ -z ${CHE_NAME} ]]; then
+      export $CHE_NAME="che"
+      echo "setting up default CHE_NAME i.e. $CHE_NAME "
+    fi
+    REGISTRATION_SERVICE_URL=$REGISTRATION_SERVICE_URL CONSOLE_NAMESPACE=$CONSOLE_NAMESPACE CHE_NAMESPACE=$CHE_NAMESPACE CHE_NAME=$CHE_NAME NAMESPACE=$HOST_OPERATOR_NS envsubst < ./config/host_operator_config.yaml | oc apply -f -
     NAME=host-operator OPERATOR_NAME=toolchain-host-operator NAMESPACE=$HOST_OPERATOR_NS envsubst < ./config/operator_deploy.yaml | oc apply -f -
     oc apply -f ./config/reg_service_route.yaml
   else
