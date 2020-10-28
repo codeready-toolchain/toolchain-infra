08d6a5ca24b9dde1a8fa0ee9dfd7530373fee00a
Xavier Coulon <xcoulon@redhat.com>
2020-10-28 15:52:25 +0100

    feat: install Grafana with Prometheus datasource
    - create a ServiceAccount with `cluster-monitoring-view` role
    - retrieve the ServiceAccount's token and use it as a bearer token
    to connect to Thanos Querier (which is itself "protected with
    an openshift/oauth-proxy sidecar container)
    
    Fixes https://issues.redhat.com/browse/CRT-809
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

---
+++ setup_grafana.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+####################################################
+## Enabling user workload monitoring
+####################################################
+function enable_user_workload_monitoring() {
+  echo "🔬  enabling User Workload Monitoring"
+  # see https://docs.openshift.com/container-platform/4.6/monitoring/configuring-the-monitoring-stack.html#preparing-to-configure-the-monitoring-stack
+  # however, we may want to improve the script a little more (ie, check if the CM exist before creating them)
+  oc apply -f config/monitoring/cluster_monitoring_config.yaml
+  oc apply -f config/monitoring/user_workload_monitoring_config.yaml
+
+  # at this point, there should be some new pods in the `openshift-user-workload-monitoring` namespace
+  oc wait --for condition=Ready pods -l app=prometheus -n openshift-user-workload-monitoring
+  echo "✅  done with enabling User Workload Monitoring"
+}
+
+####################################################
+## Deploying Grafana
+####################################################
+function deploy_grafana() {
+  echo "🚛  deploying Grafana..."
+  oc create namespace toolchain-grafana
+  oc apply -f config/monitoring/grafana_serviceaccount.yaml
+  oc adm policy add-cluster-role-to-user cluster-monitoring-view -z grafana -n toolchain-grafana
+  oc process -f config/monitoring/grafana_app.tmpl.yaml -p BEARER_TOKEN="$(oc serviceaccounts get-token grafana -n toolchain-grafana)" | oc apply -f -
+  echo "✅  done with deploying Grafana"
+  echo "🖥  https://$(oc get route/grafana -n toolchain-grafana -o json | jq -r '.status.ingress[0].host')"
+  
+}
+
+enable_user_workload_monitoring
+deploy_grafana
---
+++ setup_idp.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+function setup_idp() {
+  # setup RHD identity provider with required secret and oauth configuration
+  echo "setting up RHD identity provider"
+  if [[ -z ${ISSUER} ]]; then
+    export ISSUER="https://sso.redhat.com/auth/realms/redhat-external"
+    echo "setting up default ISSUER url i.e. $ISSUER for identity provider configuration"
+  fi
+  if [[ -z ${IDENTITY_PROVIDER} ]]; then
+    export IDENTITY_PROVIDER="rhd"
+    echo "setting up default IDENTITY_PROVIDER i.e. $IDENTITY_PROVIDER for identity provider configuration"
+  fi
+  CLIENT_SECRET=$CLIENT_SECRET envsubst <./config/oauth/rhd_idp_secret.yaml | oc apply -f -
+  IDENTITY_PROVIDER=$IDENTITY_PROVIDER ISSUER=$ISSUER envsubst <./config/oauth/idp.yaml | oc apply -f -
+}
+
+if [[ -z ${CLIENT_SECRET} ]]; then
+    echo "skipping RHD identity provider setup as environment variable 'CLIENT_SECRET' is not set"
+else
+    setup_idp
+fi
