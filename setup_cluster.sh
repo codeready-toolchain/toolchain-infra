b20a35047209a4504018f0e79098d08510e2a8ea
Alexey Kazakov <alkazako@redhat.com>
2020-11-16 18:22:43 -0800

    Remove che configs from host operator

--- setup_cluster.sh
+++ setup_cluster.sh
@@ -84,15 +84,7 @@ function deploy_operators() {
       export $CONSOLE_ROUTE_NAME="console"
       echo "setting up default CONSOLE_ROUTE_NAME i.e. $CONSOLE_ROUTE_NAME "
     fi
-    if [[ -z ${CHE_NAMESPACE} ]]; then
-      export $CHE_NAMESPACE="codeready-workspaces-operator"
-      echo "setting up default CHE_NAMESPACE i.e. $CHE_NAMESPACE "
-    fi
-    if [[ -z ${CHE_ROUTE_NAME} ]]; then
-      export $CHE_ROUTE_NAME="codeready"
-      echo "setting up default CHE_ROUTE_NAME i.e. $CHE_ROUTE_NAME "
-    fi
-    REGISTRATION_SERVICE_URL=$REGISTRATION_SERVICE_URL CONSOLE_NAMESPACE=$CONSOLE_NAMESPACE CONSOLE_ROUTE_NAME=$CONSOLE_ROUTE_NAME CHE_NAMESPACE=$CHE_NAMESPACE CHE_ROUTE_NAME=$CHE_ROUTE_NAME NAMESPACE=$HOST_OPERATOR_NS envsubst < ./config/host_operator_config.yaml | oc apply -f -
+    REGISTRATION_SERVICE_URL=$REGISTRATION_SERVICE_URL CONSOLE_NAMESPACE=$CONSOLE_NAMESPACE CONSOLE_ROUTE_NAME=$CONSOLE_ROUTE_NAME NAMESPACE=$HOST_OPERATOR_NS envsubst < ./config/host_operator_config.yaml | oc apply -f -
     NAME=host-operator OPERATOR_NAME=toolchain-host-operator NAMESPACE=$HOST_OPERATOR_NS envsubst < ./config/operator_deploy.yaml | oc apply -f -
     oc apply -f ./config/reg_service_route.yaml
   else
